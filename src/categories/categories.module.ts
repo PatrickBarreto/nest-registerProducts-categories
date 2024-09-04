f655ccd56c71cc1b0e4bab91b7a37b5bc5774fae
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-09-04 11:35:45 -0300

    Inject Prisma Module to don't use it on App.Module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './services/categories.service';
 import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesRepository } from './repository/categories.repository';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoriesRepository],
 })
